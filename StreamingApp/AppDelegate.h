988a999a874b5609e17b53eace42c556c3194cec
Tyler Much <tylermuch@gmail.com>
2014-09-01 17:10:17 -0500

    Initial Commit

---
+++ StreamingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StreamingApp
+//
+//  Created by Tyler Much on 9/1/14.
+//  Copyright (c) 2014 Tyler Much. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
