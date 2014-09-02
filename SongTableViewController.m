6f908d3198fedd36c73595d588a1f5176988d279
Tyler Much <tylermuch@gmail.com>
2014-09-02 01:28:37 -0500

    Add swiping funtionality to the cells in the Song table. As of right now there are two buttons that are revealed when the user swipes the cell to the left: Queue and +Playlist. This buttons don't actually do anything yet but they will once I have implement those features.

--- SongTableViewController.m
+++ SongTableViewController.m
@@ -7,17 +7,32 @@
 //
 
 #import "SongTableViewController.h"
+#include "SwipeableSongCell.h"
 
-@interface SongTableViewController ()
-
+@interface SongTableViewController () <SwipeableCellDelegate>
+@property (nonatomic, strong) NSMutableSet *cellsCurrentlyEditing;
 @end
 
 @implementation SongTableViewController
 {
     NSString *givenAlbum;
     NSString *givenArtist;
+    int rowSelected;
 }
 
+- (void)cellDidOpen:(UITableViewCell *)cell {
+    NSIndexPath *currentEditingIndexPath = [self.tableView indexPathForCell:cell];
+    [self.cellsCurrentlyEditing addObject:currentEditingIndexPath];
+}
+
+- (void)cellDidClose:(UITableViewCell *)cell {
+    [self.cellsCurrentlyEditing removeObject:[self.tableView indexPathForCell:cell]];
+}
+
+- (void)viewDidLoad {
+    rowSelected = -1;
+    self.cellsCurrentlyEditing = [NSMutableSet new];
+}
 
 - (void)refreshTable {
     [self populateSongsTableFromNetworkForAlbum:givenAlbum fromArtist:givenArtist];
@@ -28,6 +43,57 @@
     givenAlbum = [dict objectForKey:@"album"];
     self.title = givenAlbum;
     [self refreshTable];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [tableView beginUpdates];
+    [tableView endUpdates];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (rowSelected >= 0) {
+        if (indexPath.row == rowSelected) {
+            return 88;
+        }
+    }
+    return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    SwipeableSongCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellyCell" forIndexPath:indexPath];
+    
+    NSString *item = [self.tableItems objectAtIndex:indexPath.row];
+    cell.itemText = item;
+    
+    cell.delegate = self;
+    
+    if ([self.cellsCurrentlyEditing containsObject:indexPath]) {
+        [cell openCell];
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSLog(@"Removing object at index %ld!", (long)indexPath.row);
+    } else {
+        NSLog(@"Unhandled editing style! %ld", editingStyle);
+    }
+}
+
+#pragma mark - SwipeableCellDelegate
+- (void)buttonOneActionForItemText:(NSString *)itemText {
+    NSLog(@"In the delegate, clicked button one for %@", itemText);
+}
+
+- (void)buttonTwoActionForItemText:(NSString *)itemText {
+    NSLog(@"In the delegate, clicked button two for %@", itemText);
 }
 
 @end
