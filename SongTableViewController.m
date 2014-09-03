179935d32c77c9140efa93090e5f768eef43db0d
Tyler Much <tylermuch@gmail.com>
2014-09-02 23:11:57 -0500

    Add UIActionSheet to display options for song.
    Instead of having the swipe action reveal buttons underneath the cell, I'm going to use the UIActionSheet to display the opens when the cell is held down. Selecting the cell normally (not holding down) is still handled separately and will probably result in the song being played.

--- SongTableViewController.m
+++ SongTableViewController.m
@@ -8,7 +8,7 @@
 
 #import "SongTableViewController.h"
 
-@interface SongTableViewController ()
+@interface SongTableViewController () <UIActionSheetDelegate>
 
 @end
 
@@ -16,6 +16,21 @@
 {
     NSString *givenAlbum;
     NSString *givenArtist;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UILongPressGestureRecognizer *gesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handlePressHoldFrom:)];
+    [self.tableView addGestureRecognizer:gesture];
+}
+
+- (void)handlePressHoldFrom:(UILongPressGestureRecognizer *)recognizer {
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        CGPoint holdLocation = [recognizer locationInView:self.tableView];
+        NSIndexPath *heldIndexPath = [self.tableView indexPathForRowAtPoint:holdLocation];
+        [self tableView:self.tableView didHoldRowAtIndexPath:heldIndexPath];
+    }
 }
 
 
@@ -28,6 +43,19 @@
     givenAlbum = [dict objectForKey:@"album"];
     self.title = givenAlbum;
     [self refreshTable];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"Selected: %@", [tableView cellForRowAtIndexPath:indexPath].textLabel.text);
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    // TODO: Implement for each button
+}
+
+- (void)tableView:(UITableView *)tableView didHoldRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:[tableView cellForRowAtIndexPath:indexPath].textLabel.text  delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Play Song", @"Add to Queue", @"Add to Playlist", nil];
+    [actionSheet showFromTabBar:self.tabBarController.tabBar];
 }
 
 @end
