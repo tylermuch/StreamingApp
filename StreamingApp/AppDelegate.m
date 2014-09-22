d357325e2623a8220fa103b6afb0c56ab96db440
Tyler Much <tylermuch@gmail.com>
2014-09-21 23:22:44 -0500

    Enable background audio playing.
    Audio will now play when the app is in the background and when the phone is locked.

--- StreamingApp/AppDelegate.m
+++ StreamingApp/AppDelegate.m
@@ -7,12 +7,17 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setActive: YES error: nil];
+    
     return YES;
 }
 							
