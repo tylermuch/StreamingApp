894875dba1a1bb5efb19b5caab4bdf32c6e5969b
Tyler Much <tylermuch@gmail.com>
2014-09-23 18:06:55 -0500

    Implement token renewal and storing session in NSUserDefaults.
    Also added a bunch of logging.

--- StreamingApp/AppDelegate.m
+++ StreamingApp/AppDelegate.m
@@ -14,10 +14,46 @@ static NSString * const kClientId = @"9d29a3c980c4430c89047224e3d6cfa3";
 static NSString * const kCallbackURL = @"streaming-app://callback";
 static NSString * const kTokenSwapServiceURL = @"http://tylermuch.com:5001/swap"; // token exchange service
 static NSString * const kTokenRefreshServiceURL = @"http://tylermuch.com:1234/refresh"; // token refresh service
+static NSString * const kSessionUserDefaultsKey = @"AppStreamingSpotifySession";
 
 @implementation AppDelegate
 
+- (void)storeSessionInUserDefaults:(SPTSession *)session {
+    NSLog(@"Storing session in user defaults");
+    NSData *sessionData = [NSKeyedArchiver archivedDataWithRootObject:session];
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:sessionData forKey:kSessionUserDefaultsKey];
+    [userDefaults synchronize];
+}
+
+- (void)initializeSpotifySession {
+    NSLog(@"Initializing spotify session.");
+    
+    id sessionData = [[NSUserDefaults standardUserDefaults] objectForKey:kSessionUserDefaultsKey];
+    SPTSession *session = sessionData ? [NSKeyedUnarchiver unarchiveObjectWithData:sessionData] : nil;
+    
+    if (session) {
+        NSLog(@"Found session in NSUserDefaults.");
+        if ([session isValid]) {
+            NSLog(@"Session is valid.");
+            [self storeSessionInUserDefaults:session];
+            [[AudioManager sharedInstance] setSpotifySession:session];
+        } else {
+            NSLog(@"Session invalid. Refreshing.");
+            if (kTokenRefreshServiceURL == nil || [kTokenRefreshServiceURL isEqualToString:@""]) {
+                [self openLoginPage];
+            } else {
+                [self renewToken];
+            }
+        }
+    } else {
+        NSLog(@"Did not find session in NSUserDefaults.");
+        [self openLoginPage];
+    }
+}
+
 - (void)openLoginPage {
+    NSLog(@"Opening login page.");
     SPTAuth *auth = [SPTAuth defaultInstance];
     
     NSURL *loginURL;
@@ -30,8 +66,27 @@ static NSString * const kTokenRefreshServiceURL = @"http://tylermuch.com:1234/re
     double delayInSeconds = 0.1;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^{
+        NSLog(@"Opening loginURL");
         [[UIApplication sharedApplication] openURL:loginURL];
     });
+}
+
+- (void)renewToken {
+    NSLog(@"renewing token");
+    id sessionData = [[NSUserDefaults standardUserDefaults] objectForKey:kSessionUserDefaultsKey];
+    SPTSession *session = sessionData ? [NSKeyedUnarchiver unarchiveObjectWithData:sessionData] : nil;
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    
+    NSLog(@"renewing session");
+    [auth renewSession:session withServiceEndpointAtURL:[NSURL URLWithString:kTokenRefreshServiceURL] callback:^(NSError *error, SPTSession *session) {
+        if (error) {
+            NSLog(@"*** Error renewing session: %@", error);
+            return;
+        }
+        [self storeSessionInUserDefaults:session];
+        NSLog(@"setting spotify session");
+        [[AudioManager sharedInstance] setSpotifySession:session];
+    }];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
@@ -40,27 +95,33 @@ static NSString * const kTokenRefreshServiceURL = @"http://tylermuch.com:1234/re
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
     [[AVAudioSession sharedInstance] setActive: YES error: nil];
     
-    // TODO: Check for existing session in NSUserDefaults
-    [self openLoginPage];
+    [self initializeSpotifySession];
     
     return YES;
 }
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    NSLog(@"application:openURL:sourceApplication:annotation:");
     SPTAuthCallback authCallback = ^(NSError *error, SPTSession *session) {
+        NSLog(@"auth callback.");
         if (error != nil) {
             NSLog(@"*** Auth error: %@", error);
             return;
         }
-        
+        NSLog(@"setting spotify session");
+        [self storeSessionInUserDefaults:session];
         [[AudioManager sharedInstance] setSpotifySession:session];
     };
     
+    
     if ([[SPTAuth defaultInstance] canHandleURL:url withDeclaredRedirectURL:[NSURL URLWithString:kCallbackURL]]) {
+        NSLog(@"Spotify auth can handle URL");
         if (kTokenSwapServiceURL == nil || [kTokenSwapServiceURL isEqualToString:@""]) {
+            NSLog(@"handle auth callback");
             [[SPTAuth defaultInstance] handleAuthCallbackWithTriggeredAuthURL:url callback:authCallback];
         } else {
             // If we have a token exchange service, we'll call it and get the token.
+            NSLog(@"handle auth callback with token swap");
             [[SPTAuth defaultInstance] handleAuthCallbackWithTriggeredAuthURL:url
                                                 tokenSwapServiceEndpointAtURL:[NSURL URLWithString:kTokenSwapServiceURL]
                                                                      callback:authCallback];
